0d611bae7143c5e7d91290ddec21f62b7f158401
Li Jar <davidbeljar@gmail.com>
2024-07-09 10:25:36 +0900

    merge

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,6 +1,9 @@
 package jpabook.jpashop.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 
 @Entity
 public class Member {
@@ -8,9 +11,13 @@ public class Member {
     @Id @GeneratedValue
     @Column(name = "MEMBER_ID")
     private Long id;
+
     private String name;
+
     private String city;
+
     private String street;
+
     private String zipcode;
 
     public Long getId() {
